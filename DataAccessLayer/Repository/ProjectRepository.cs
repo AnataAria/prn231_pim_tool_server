82ac203073d69a120a84db75c6a06960f1945212
haiha0402 <nguyenha24bmt@gmail.com>
2024-11-12 20:16:22 +0700

    ha/employee functions

---
+++ DataAccessLayer/Repository/ProjectRepository.cs
@@ -0,0 +1,25 @@
+﻿using DataAccessLayer.BusinessObject;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Repository
+{
+    public class ProjectRepository(PIMDatabaseContext context) : BaseRepository<Project, PIMDatabaseContext>(context)
+    {
+        public async Task<bool> IsEmployeeInProjectsAsync(long employeeId)
+        {
+            var list =  await _context.Projects
+            .Include(p => p.Employees)
+            .FirstOrDefaultAsync(p => p.Employees.Any(e => e.Id == employeeId));
+            if (list == null)
+                return false;
+            return true;
+
+        }
+
+    }
+}
